7559619cb9c98d12faf2c1e08348b9530f1817f4
Christoph Wendt <chwend@microsoft.com>
2015-07-15 12:59:40 -0700

    Create binding for telemetry related methods

--- ExampleGame/Assets/HockeyAppUnityIOS/HockeyAppUnityWrapper.m
+++ ExampleGame/Assets/HockeyAppUnityIOS/HockeyAppUnityWrapper.m
@@ -1,62 +1,114 @@
 #import "HockeyAppUnity.h"
 #import "HockeyAppUnityWrapper.h"
 
+// Helper
+char* createStringCopy(const char *string){
+	char* res = (char*)malloc(strlen(string) + 1);
+	strcpy(res, string);
+	return res;
+}
+
 void HockeyApp_StartHockeyManager(char *appID, char *serverURL, char *authType, char *secret, bool updateManagerEnabled, bool autoSendEnabled) {
-  
   [HockeyAppUnity startManagerWithIdentifier:[NSString stringWithUTF8String:appID]
                                    serverURL:[NSString stringWithUTF8String:serverURL]
                                     authType:[NSString stringWithUTF8String:authType]
                                       secret:[NSString stringWithUTF8String:secret]
                         updateManagerEnabled:updateManagerEnabled
                              autoSendEnabled:autoSendEnabled];
-}
+}d
 
 void HockeyApp_ShowFeedbackListView() {
-  
   [HockeyAppUnity showFeedbackListView];
 }
 
 char* HockeyApp_GetVersionCode() {
-  
   const char* versionCode = [[HockeyAppUnity versionCode] UTF8String];
-  char* res = (char*)malloc(strlen(versionCode) + 1);
-  strcpy(res, versionCode);
-  
-  return res;
+  return createStringCopy(versionCode);
 }
 
 char* HockeyApp_GetVersionName() {
-  
   const char* versionName = [[HockeyAppUnity versionName] UTF8String];
-  char* res = (char*)malloc(strlen(versionName) + 1);
-  strcpy(res, versionName);
-  
-  return res;
+  return createStringCopy(versionName);
 }
 
 char* HockeyApp_GetSdkVersion() {
-  
   const char* sdkVersion = [[HockeyAppUnity sdkVersion] UTF8String];
-  char* res = (char*)malloc(strlen(sdkVersion) + 1);
-  strcpy(res, sdkVersion);
-  
-  return res;
+  return createStringCopy(sdkVersion);
 }
 
 char* HockeyApp_GetSdkName() {
-  
   const char* sdkName = [[HockeyAppUnity sdkName] UTF8String];
-  char* res = (char*)malloc(strlen(sdkName) + 1);
-  strcpy(res, sdkName);
-  
-  return res;
+  return createStringCopy(sdkName);
 }
 
 char* HockeyApp_GetBundleIdentifier() {
-  
   const char* bundleIdentifier = [[HockeyAppUnity bundleIdentifier] UTF8String];
-  char* res = (char*)malloc(strlen(bundleIdentifier) + 1);
-  strcpy(res, bundleIdentifier);
-  
-  return res;
+  return createStringCopy(bundleIdentifier);
 }
+
+//Telemetry
+
+void HockeyApp_setCommonProperties(char *properties){
+	[HockeyAppUnity setCommonProperties:[NSString stringWithUTF8String:properties]];
+}
+
+void HockeyApp_trackEvent1(char *eventName) {
+	[HockeyAppUnity trackEventWithName:[NSString stringWithUTF8String:eventName]];
+}
+
+void HockeyApp_trackEvent2(char *eventName, char *properties) {
+	[HockeyAppUnity trackEventWithName:[NSString stringWithUTF8String:eventName]
+													properties:[NSString stringWithUTF8String:properties]];
+}
+
+void HockeyApp_trackTrace1(char *message) {
+	[HockeyAppUnity trackTraceWithMessage:[NSString stringWithUTF8String:message]];
+}
+
+void HockeyApp_trackTrace2(char *message, char *properties) {
+	[HockeyAppUnity trackTraceWithMessage:[NSString stringWithUTF8String:message]
+														 properties:[NSString stringWithUTF8String:properties]];
+}
+
+void HockeyApp_trackPageView1(char *pageName) {
+	[HockeyAppUnity trackPageView:[NSString stringWithUTF8String:pageName]];
+}
+
+void HockeyApp_trackPageView2(char *pageName, long duration) {
+	[HockeyAppUnity trackPageView:[NSString stringWithUTF8String:pageName]
+											 duration:duration];
+}
+
+void HockeyApp_trackPageView3(char *pageName, long duration, char *properties) {
+	[HockeyAppUnity trackPageView:[NSString stringWithUTF8String:pageName]
+											 duration:duration
+										 properties:[NSString stringWithUTF8String:properties]];
+}
+
+void HockeyApp_setAutoPageViewTrackingDisabled(bool autoPageViewTrackingDisabled){
+	[HockeyAppUnity setAutoPageViewTrackingDisabled:autoPageViewTrackingDisabled];
+}
+
+void HockeyApp_setAutoSessionManagementDisabled(bool autoSessionManagementDisabled){
+	[HockeyAppUnity setAutoSessionManagementDisabled:autoSessionManagementDisabled];
+}
+
+void HockeyApp_setServerURL(char *serverURL){
+	[HockeyAppUnity setServerURL:[NSString stringWithUTF8String:serverURL]];
+}
+
+void HockeyApp_startNewSession(){
+	[HockeyAppUnity startNewSession];
+}
+
+void HockeyApp_setAppBackgroundTimeBeforeSessionExpires(int appBackgroundTime){
+	[HockeyAppUnity setAppBackgroundTimeBeforeSessionExpires:appBackgroundTime];
+}
+
+void HockeyApp_renewSession(char *sessionId){
+	[HockeyAppUnity renewSessionWithId:[NSString stringWithUTF8String:sessionId]];
+}
+
+
+
+
